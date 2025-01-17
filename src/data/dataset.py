aa6efd6edbf8ac20683f14d2abb5de73709cfa1f
hexiheng <simpsonhxh@hotmail.com>
2025-01-17 18:06:25 +0100

    add dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import torch
 from pyfaidx import Fasta
+from tqdm import tqdm
 
 from datasets import Dataset, Features, Value
 from src.utils.config import get_params
@@ -110,25 +111,27 @@ class HuggingFaceDatasetCreator:
             test_size (float, optional): Test set size. Defaults to 0.1.
         """
         self.sequence = Fasta(sequence, as_raw=True)
-        self.metadata = pd.read_csv(metadata, sep="\t", usecols=["EntryID", "GO_terms"])
+        self.metadata = pd.read_csv(metadata, sep="\t", usecols=["EntryID", "GO_terms", "positive_GO", "negative_GO"])
         self.sentence = pd.read_csv(go_sentence, sep="\t").set_index("term")["sentence"].to_dict()
         self.seed = seed
         self.test_size = test_size
 
-    def concatenate_sentences(self, go_terms: str) -> str:
+    def concatenate_sentences(self, go_terms: list, delimiter: str, start: str = "", end: str = "") -> str:
         """
         Concatenate GO sentences for given GO terms.
 
         Args:
-            go_terms (str): Comma-separated GO terms.
+            go_terms (list): List of GO terms.
+            delimiter (str): Delimiter to join the sentences.
+            start (str, optional): Start of the concatenated sentence. Defaults to "".
+            end (str, optional): End of the concatenated sentence. Defaults to "".
 
         Returns:
             str: Concatenated GO sentences.
         """
-        terms = go_terms.split(",")
-        sentences = [self.sentence[term] for term in terms]
+        sentences = [self.sentence[term] for term in go_terms]
         sentences = [s for s in sentences if s]
-        return ", ".join(sentences) + "."
+        return start + delimiter.join(sentences) + end
 
     def create_dataset(self) -> Dataset:
         """
@@ -137,12 +140,99 @@ class HuggingFaceDatasetCreator:
         Returns:
             Dataset: HuggingFace Dataset object.
         """
-        concatenated = [self.concatenate_sentences(go_terms) for go_terms in self.metadata["GO_terms"].tolist()]
-        sequences = [str(self.sequence[prot]) for prot in self.metadata["EntryID"].tolist()]
+        raise NotImplementedError
+
+
+class ProteinGOConcatenatedDataset(HuggingFaceDatasetCreator):
+    def __init__(self, sequence: Path, metadata: Path, go_sentence: Path, seed: int = 42, test_size: float = 0.1):
+        super().__init__(sequence, metadata, go_sentence, seed, test_size)
+
+    def create_dataset(self) -> Dataset:
+        """
+        Create a protein-GO concatenated dataset by concatenating only `true` GO sentences for each protein.
+
+        Returns:
+            Dataset: HuggingFace Dataset object.
+        """
+        concatenated = [
+            self.concatenate_sentences(go_terms, delimiter=",", end=".")
+            for go_terms in tqdm(self.metadata["GO_terms"].str.split(",").to_list(), desc="Concatenating sentences")
+        ]
 
         data = {
             "proteins": self.metadata["EntryID"].tolist(),
-            "sequences": sequences,
+            "sequences": [str(self.sequence[prot]) for prot in self.metadata["EntryID"].tolist()],
+            "sentences": concatenated,
+        }
+
+        features = Features({"proteins": Value("string"), "sequences": Value("string"), "sentences": Value("string")})
+
+        return (
+            Dataset.from_dict(data, features=features)
+            .shuffle(seed=self.seed)
+            .train_test_split(test_size=self.test_size)
+        )
+
+
+class ProteinGOContrastiveConcatenatedDataset(HuggingFaceDatasetCreator):
+    def __init__(self, sequence: Path, metadata: Path, go_sentence: Path, seed: int = 42, test_size: float = 0.1):
+        super().__init__(sequence, metadata, go_sentence, seed, test_size)
+        self.rng = np.random.default_rng(seed)
+
+    def sample_negative(self, positive_go_terms: list, go_terms: list) -> list:
+        """
+        Sample negative GO terms for a given protein.
+
+        Args:
+            positive_go_terms (list): List of positive GO terms.
+            go_terms (list): list of all GO terms which already redundancy reduced to ensure their highly specificity.
+
+        Returns:
+            list: List of negative GO terms.
+        """
+
+        sampled = list(self.rng.choice(list(set(go_terms) - set(positive_go_terms)), size=10, replace=False))
+
+        """
+        # If we don't have enough terms, sample more until we get 10
+        remaining = 10 - len(negative_go_terms)
+        if remaining > 0:
+            available_terms = go_terms - set(positive_go_terms) - negative_go_terms
+            if available_terms:
+                additional = set(
+                    rng.choice(list(available_terms), size=min(remaining, len(available_terms)), replace=False)
+                )
+                negative_go_terms.update(additional)
+        """
+
+        return sampled
+
+    def create_dataset(self) -> Dataset:
+        """
+        Create a protein-GO concatenated dataset by concatenating both `true` and `negative` GO sentences for each protein.
+
+        Returns:
+            Dataset: HuggingFace Dataset object.
+        """
+
+        go_terms_pool = self.metadata["positive_GO"].str.split(",").explode().unique().tolist()
+
+        concatenated = []
+        for go_terms in tqdm(
+            self.metadata["GO_terms"].str.split(",").to_list(), desc="Concatenating contrasive sentences"
+        ):
+            positive = self.concatenate_sentences(go_terms, delimiter=",", start="Relevant to: ", end=";")
+            negative = self.concatenate_sentences(
+                self.sample_negative(go_terms, go_terms_pool),
+                delimiter=",",
+                start="Not relevant to: ",
+                end=".",
+            )
+            concatenated.append(" ".join([positive, negative]))
+
+        data = {
+            "proteins": self.metadata["EntryID"].tolist(),
+            "sequences": [str(self.sequence[prot]) for prot in self.metadata["EntryID"].tolist()],
             "sentences": concatenated,
         }
 
@@ -158,7 +248,12 @@ class HuggingFaceDatasetCreator:
 def main():
     params = get_params("dataset")
 
-    creator = HuggingFaceDatasetCreator(
+    try:
+        dataset_class = globals()[params["type"]]
+    except KeyError:
+        raise ValueError("Unknown dataset class.")
+
+    creator = dataset_class(
         Path(params["protein"]),
         Path(params["metadata"]),
         Path(params["go_sentence"]),
@@ -167,7 +262,7 @@ def main():
     )
 
     dataset = creator.create_dataset()
-    dataset.save_to_disk(params["output"])
+    dataset.save_to_disk(Path(params["out"]))
 
 
 if __name__ == "__main__":
